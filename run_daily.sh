e6c9b60d208978694947d707e733b6a90e3e85db
MrLoLf <roscher2003@outlook.de>
2024-08-29 17:17:07 +0200

    updated daily run to use shell script

--- run_daily.sh
+++ run_daily.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 cd /home/server/HVSA-Website-scraper
 /usr/bin/python3 main.py
+if [ ! -d ".venv" ]; then
+    # Create a virtual environment
+    /usr/bin/python3 -m venv .venv
+fi
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+# Install the requirements
+pip install -r requirements.txt
+
+# Run the Python script
+python main.py
+
+# Deactivate the virtual environment
+deactivate
